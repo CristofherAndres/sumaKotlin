f344c2b83dff9d16e81b64f9fa249e62d91fcca3
Cristofher <Cristofherrojas.cr@gmail.com>
2024-09-09 17:30:29 -0300

    Initial commit

---
+++ app/src/main/java/com/example/sumakotlin/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.sumakotlin
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun sumar(view: View){
+        Log.e("BOTON","HICE CLICK")
+
+        val num1 = findViewById<TextInputEditText>(R.id.etNumero1).text
+        // num1 = num1.toString().toInt()
+        Log.e("NUMERO 1", "El numero 1 es: $num1");
+
+    }
+
+}
